0b92e21d1c3ae46fb4e7ef6565684b45a59b450e
Jordan Jordanov <d_yugioh@abv.bg>
2019-07-11 10:34:26 +0300

    sports-system v1.1.3

--- server/src/Jbet.Persistence/EntityFramework/OnModelCreatingConfiguration.cs
+++ server/src/Jbet.Persistence/EntityFramework/OnModelCreatingConfiguration.cs
@@ -37,5 +37,84 @@ namespace Jbet.Persistence.EntityFramework
                 .Property(p => p.Id)
                 .ValueGeneratedOnAdd();
         }
+
+        internal static void ConfigureUserCommentsRelations(this ModelBuilder builder)
+        {
+            builder
+                .Entity<Comment>()
+                .HasOne(comment => comment.User)
+                .WithMany(user => user.Comments)
+                .HasForeignKey(comment => comment.UserId);
+        }
+
+        internal static void ConfigureMatchCommentsRelations(this ModelBuilder builder)
+        {
+            builder
+                .Entity<Comment>()
+                .HasOne(comment => comment.Match)
+                .WithMany(match => match.Comments)
+                .HasForeignKey(comment => comment.MatchId);
+        }
+
+        internal static void ConfigureTeamMatchRelations(this ModelBuilder builder)
+        {
+            builder
+                .Entity<Match>()
+                .HasOne(match => match.AwayTeam)
+                .WithMany(team => team.AwayMatches)
+                .HasForeignKey(match => match.AwayTeamId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .Entity<Match>()
+                .HasOne(match => match.HomeTeam)
+                .WithMany(team => team.HomeMatches)
+                .HasForeignKey(match => match.HomeTeamId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
+        internal static void ConfigureTeamPlayerRelations(this ModelBuilder builder)
+        {
+            builder
+                .Entity<Player>()
+                .HasOne(player => player.Team)
+                .WithMany(team => team.Players)
+                .HasForeignKey(player => player.TeamId)
+                .IsRequired(false);
+        }
+
+        internal static void ConfigureUserMatchBetsRelations(this ModelBuilder builder)
+        {
+            builder
+                .Entity<UserMatchBet>()
+                .HasOne(bet => bet.User)
+                .WithMany(user => user.UserMatchBets)
+                .HasForeignKey(bet => bet.UserId)
+                .IsRequired();
+
+            builder
+                .Entity<UserMatchBet>()
+                .HasOne(bet => bet.Match)
+                .WithMany(match => match.UserMatchBets)
+                .HasForeignKey(bet => bet.MatchId)
+                .IsRequired();
+        }
+
+        internal static void ConfigureUserTeamVotesRelations(this ModelBuilder builder)
+        {
+            builder
+                .Entity<Vote>()
+                .HasOne(vote => vote.User)
+                .WithMany(user => user.UserTeamVotes)
+                .HasForeignKey(vote => vote.UserId)
+                .IsRequired();
+
+            builder
+                .Entity<Vote>()
+                .HasOne(vote => vote.Team)
+                .WithMany(team => team.Votes)
+                .HasForeignKey(vote => vote.TeamId)
+                .IsRequired();
+        }
     }
 }
