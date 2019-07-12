58131b6df574712aa1189dd29ccb00a320c62049
Jordan Jordanov <d_yugioh@abv.bg>
2019-07-12 09:06:16 +0300

    sports-system v1.2.3

---
+++ server/src/Jbet.Api/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Jbet.Core.AuthContext;
+using Jbet.Core.AuthContext.Commands;
+using Jbet.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jbet.Api.Controllers
+{
+    public class AuthController : Controller
+    {
+        /// <summary>
+        /// Retrieves the currently logged in user.
+        /// </summary>
+        [HttpGet(Name = nameof(GetCurrentUser))]
+        public async Task<IActionResult> GetCurrentUser()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Register.
+        /// </summary>
+        /// <param name="command">The user model.</param>
+        /// <returns>A user model.</returns>
+        /// <response code="201">A user was created.</response>
+        /// <response code="400">Invalid input.</response>
+        [HttpPost("register", Name = nameof(Register))]
+        [ProducesResponseType(typeof(Error), (int) HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Register([FromBody] Register command)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Logout. (unsets the auth cookie)
+        /// </summary>
+        [Authorize]
+        [HttpDelete("logout", Name = nameof(Logout))]
+        public async Task<IActionResult> Logout()
+        {
+            HttpContext.Response.Cookies.Delete(AuthConstants.Cookies.AuthCookieName);
+            //var resource = await ToEmptyResourceAsync<LogoutResource>();
+            return Ok();
+        }
+    }
+}
---
+++ server/src/Jbet.Api/Hateoas/Resources/Auth/LoginResourcePolicy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Jbet.Api.Controllers;
+using Jbet.Api.Hateoas.Resources.Base;
+using RiskFirst.Hateoas;
+
+namespace Jbet.Api.Hateoas.Resources.Auth
+{
+    public class LoginResourcePolicy : IPolicy<LoginResource>
+    {
+        public Action<LinksPolicyBuilder<LoginResource>> PolicyConfiguration => policy =>
+        {
+            policy.RequireSelfLink();
+            policy.RequireRoutedLink(LinkNames.Auth.GetCurrentUser, nameof(AuthController.GetCurrentUser));
+            policy.RequireRoutedLink(LinkNames.Auth.Logout, nameof(AuthController.Logout));
+        };
+    }
+}
