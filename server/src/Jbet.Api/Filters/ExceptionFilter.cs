3cc357be287cc602c2b869b4e93ae6b19e4f714e
Jordan Jordanov <d_yugioh@abv.bg>
2019-07-12 08:45:43 +0300

    sports-system v1.2.0

---
+++ server/src/Jbet.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,30 @@
+﻿using Jbet.Domain;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace Jbet.Api.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public ExceptionFilter(IHostingEnvironment environment)
+        {
+            _hostingEnvironment = environment;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            var status = (int)HttpStatusCode.InternalServerError;
+
+            var result = _hostingEnvironment.IsDevelopment() ?
+                new JsonResult(context.Exception) :
+                new JsonResult(Error.Critical("An unexpected internal server error has occurred."));
+
+            context.HttpContext.Response.StatusCode = status;
+            context.Result = result;
+        }
+    }
+}
