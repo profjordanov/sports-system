fc6ea62d3dbc3aedf81c08c3379e116562e72db3
Jordan Jordanov <d_yugioh@abv.bg>
2019-07-13 18:57:03 +0300

    restaurant v2.4.2

---
+++ server/src/Jbet.Api/Filters/EntityFrameworkTransactionFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Jbet.Persistence.EntityFramework;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Jbet.Api.Filters
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Starts an Entity Framework transaction before each action and commits it afterwards.
+    /// </summary>
+    public class EntityFrameworkTransactionFilter : IAsyncActionFilter
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public EntityFrameworkTransactionFilter(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <inheritdoc />
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            using (var transaction = await _dbContext.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    await next();
+
+                    transaction.Commit();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
