dd53dca19bfd0c8a2c48ec1a0a2c94e9587c2e98
Jordan Jordanov <d_yugioh@abv.bg>
2019-07-15 07:49:15 +0300

    sports-system v1.3.7

---
+++ server/src/Jbet.Core/PlayerContext/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Jbet.Domain.Entities;
+using Jbet.Domain.Views.Player;
+
+namespace Jbet.Core.PlayerContext
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Player, PlayerView>(MemberList.Destination);
+        }
+    }
+}
