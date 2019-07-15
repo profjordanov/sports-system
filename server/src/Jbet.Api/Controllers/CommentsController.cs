d83f222ca319f66d097ab39e036dce3f5af7c074
Jordan Jordanov <d_yugioh@abv.bg>
2019-07-15 11:36:59 +0300

    sports-system v1.4.9

---
+++ server/src/Jbet.Api/Controllers/CommentsController.cs
@@ -0,0 +1,17 @@
+﻿using Jbet.Api.Hateoas.Resources.Base;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jbet.Api.Controllers
+{
+    /// <summary>
+    /// A controller responsible for comments for matches.
+    /// </summary>
+    public class CommentsController : ApiController
+    {
+        public CommentsController(IMediator mediator, IResourceMapper resourceMapper)
+            : base(mediator, resourceMapper)
+        {
+        }
+    }
+}
