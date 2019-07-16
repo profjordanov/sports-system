e4bdc7e9e60fb26f6e047b1f327338ebcd51373e
Jordan Jordanov <d_yugioh@abv.bg>
2019-07-16 08:53:39 +0300

    sports-system v1.5.4

---
+++ server/src/Jbet.Domain/Repositories/IBetsRepository.cs
@@ -0,0 +1,25 @@
+﻿using Jbet.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace Jbet.Domain.Repositories
+{
+    public interface IBetsRepository
+    {
+        Task<UserMatchBet> AddHomeBetAsync(
+            Guid userId,
+            Guid matchId,
+            decimal homeBet);
+
+        Task<UserMatchBet> AddAwayBetAsync(
+            Guid userId,
+            Guid matchId,
+            decimal awayBet);
+
+        Task<UserMatchBet> AddAsync(
+            Guid userId,
+            Guid matchId,
+            decimal homeBet,
+            decimal awayBet);
+    }
+}
---
+++ server/src/Jbet.Persistence/Repositories/BetsRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Jbet.Domain.Entities;
+using Jbet.Domain.Repositories;
+using Jbet.Persistence.EntityFramework;
+
+namespace Jbet.Persistence.Repositories
+{
+    public class BetsRepository : IBetsRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public BetsRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<UserMatchBet> AddHomeBetAsync(
+            Guid userId,
+            Guid matchId,
+            decimal homeBet) =>
+            AddAsync(userId, matchId, homeBet, 0);
+
+        public Task<UserMatchBet> AddAwayBetAsync(
+            Guid userId,
+            Guid matchId,
+            decimal awayBet) =>
+            AddAsync(userId, matchId, 0, awayBet);
+
+        public async Task<UserMatchBet> AddAsync(
+            Guid userId,
+            Guid matchId,
+            decimal homeBet,
+            decimal awayBet)
+        {
+            var entity = new UserMatchBet
+            {
+                Id = Guid.NewGuid(),
+                UserId = userId,
+                MatchId = matchId,
+                AwayBet = awayBet,
+                HomeBet = homeBet
+            };
+
+            await _dbContext.UserMatchBets.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+    }
+}
