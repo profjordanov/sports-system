2dbf7e73d2f2fa7643e70422aedd8f252eee9d75
Jordan Jordanov <d_yugioh@abv.bg>
2019-07-16 22:35:28 +0300

    sports-system v1.5.6

---
+++ server/tests/Jbet.Tests/AppFixture.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Jbet.Api;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jbet.Tests
+{
+    public class AppFixture
+    {
+        public static readonly string BaseUrl;
+        private static readonly IConfiguration _configuration;
+        private static readonly IServiceScopeFactory _scopeFactory;
+
+        static AppFixture()
+        {
+            BaseUrl = $"http://localhost:{GetFreeTcpPort()}";
+
+            var webhost = Program
+                .CreateWebHostBuilder(new string[] { "--environment", "IntegrationTests" }, BaseUrl)
+                .Build();
+
+            webhost.Start();
+
+            var scopeFactory = (IServiceScopeFactory)webhost.Services.GetService(typeof(IServiceScopeFactory));
+
+            _scopeFactory = scopeFactory;
+
+            using (var scope = scopeFactory.CreateScope())
+            {
+                _configuration = scope.ServiceProvider.GetService<IConfiguration>();
+            }
+        }
+
+        public static string EventStoreConnectionString => _configuration.GetSection("EventStore")["ConnectionString"];
+        public static string RelationalDbConnectionString => _configuration.GetConnectionString("DefaultConnection");
+
+        private static int GetFreeTcpPort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+            return port;
+        }
+    }
+}
