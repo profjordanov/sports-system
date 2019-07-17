b1b94902621829b32a5980105737208f40662124
Jordan Jordanov <d_yugioh@abv.bg>
2019-07-17 22:00:48 +0300

    sports-system v1.5.8

--- server/tests/Jbet.Tests/AppFixture.cs
+++ server/tests/Jbet.Tests/AppFixture.cs
@@ -1,11 +1,20 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 using Jbet.Api;
+using Jbet.Core.Base;
+using Jbet.Persistence.EntityFramework;
+using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Jbet.Tests
 {
+    /// <summary>
+    /// This is the core of the integration tests.
+    /// </summary>
     public class AppFixture
     {
         public static readonly string BaseUrl;
@@ -35,6 +44,121 @@ namespace Jbet.Tests
         public static string EventStoreConnectionString => _configuration.GetSection("EventStore")["ConnectionString"];
 
         public static string RelationalDbConnectionString => _configuration.GetConnectionString("DefaultConnection");
+
+        public Task ExecuteDbContextAsync(Func<ApplicationDbContext, Task> action) =>
+            ExecuteScopeAsync(sp =>
+            {
+                var dbContext = sp.GetService<ApplicationDbContext>();
+
+                return action(dbContext);
+            });
+
+        public async Task ExecuteScopeAsync(Func<IServiceProvider, Task> action)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                try
+                {
+                    await action(scope.ServiceProvider).ConfigureAwait(false);
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public async Task<T> ExecuteScopeAsync<T>(Func<IServiceProvider, Task<T>> action)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                try
+                {
+                    var result = await action(scope.ServiceProvider).ConfigureAwait(false);
+
+                    return result;
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public async Task ExecuteServiceAsync<TService>(Func<TService, Task> action)
+        {
+            var service = await ExecuteScopeAsync(async sp => sp.GetRequiredService<TService>());
+            await action(service);
+        }
+
+        public Task<TResult> ExecuteHttpClientAsync<TResult>(Func<HttpClient, Task<TResult>> action, string accessToken = null)
+        {
+            var client = BuildHttpClient(accessToken);
+            return action(client);
+        }
+
+        public Task ExecuteHttpClientAsync(Func<HttpClient, Task> action, string accessToken = null)
+        {
+            var client = BuildHttpClient(accessToken);
+            return action(client);
+        }
+
+        public string GetCompleteServerUrl(string route)
+        {
+            route = route.TrimStart('/', '\\');
+            return $"{BaseUrl}/{route}";
+        }
+
+
+        public Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+        {
+            return ExecuteScopeAsync(sp =>
+            {
+                var mediator = sp.GetService<IMediator>();
+
+                return mediator.Send(request);
+            });
+        }
+
+        public Task SendAsync(IRequest request)
+        {
+            return ExecuteScopeAsync(sp =>
+            {
+                var mediator = sp.GetService<IMediator>();
+
+                return mediator.Send(request);
+            });
+        }
+
+        public Task SendManyAsync(params ICommand[] commands)
+        {
+            return ExecuteScopeAsync(async sp =>
+            {
+                var mediator = sp.GetService<IMediator>();
+
+                foreach (var command in commands)
+                {
+                    await mediator.Send(command);
+                }
+
+                return Task.CompletedTask;
+            });
+        }
+
+        private static HttpClient BuildHttpClient(string accessToken = null)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(BaseUrl)
+            };
+
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
+            }
+
+            return client;
+        }
 
         private static int GetFreeTcpPort()
         {
