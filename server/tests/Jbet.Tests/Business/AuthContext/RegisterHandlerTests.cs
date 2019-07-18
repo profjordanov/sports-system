a9620dc249a2004f631b2a64b6fea4d5563ec3fb
Jordan Jordanov <d_yugioh@abv.bg>
2019-07-18 07:30:39 +0300

    sports-system v1.6.5

--- server/tests/Jbet.Tests/Business/AuthContext/RegisterHandlerTests.cs
+++ server/tests/Jbet.Tests/Business/AuthContext/RegisterHandlerTests.cs
@@ -1,7 +1,35 @@
-﻿namespace Jbet.Tests.Business.AuthContext
+﻿using System.Threading.Tasks;
+using Jbet.Core.AuthContext.Commands;
+using Jbet.Domain;
+using Jbet.Tests.Customizations;
+using Jbet.Tests.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using Xunit;
+
+namespace Jbet.Tests.Business.AuthContext
 {
     public class RegisterHandlerTests : ResetDatabaseLifetime
     {
-        
+        private readonly AppFixture _fixture;
+
+        public RegisterHandlerTests()
+        {
+            _fixture = new AppFixture();
+        }
+
+        [Theory]
+        [CustomizedAutoData]
+        public async Task CannotRegisterWithInvalidEmail(Register command)
+        {
+            // Arrange
+            command.Email = "invalid-email";
+
+            // Act
+            var result = await _fixture.SendAsync(command);
+
+            // Assert
+            result.ShouldHaveErrorOfType(ErrorType.Validation);
+        }
     }
 }
