957d4541d5ae8d62cbf161c3ae0509f29344f752
Jordan Jordanov <d_yugioh@abv.bg>
2019-07-19 17:23:05 +0300

    sports-system v1.7.1

---
+++ server/tests/Jbet.Tests/Api/Configuration/DatabaseConfigurationTests.cs
@@ -0,0 +1,77 @@
+﻿using Jbet.Api.Configuration;
+using Jbet.Core.AuthContext.Commands;
+using Jbet.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Jbet.Tests.Api.Configuration
+{
+    public class DatabaseConfigurationTests : ResetDatabaseLifetime
+    {
+        private readonly AppFixture _fixture;
+
+        public DatabaseConfigurationTests()
+        {
+            _fixture = new AppFixture();
+        }
+
+        [Fact]
+        public async Task ShouldCreateDefaultAdminAccountWhenNotExisting()
+        {
+            // Arrange
+            var configuration = await _fixture.ExecuteScopeAsync(async sp => sp.GetService<IConfiguration>());
+
+            var expectedAdminCredentials = configuration.GetAdminCredentials();
+
+            // Act
+            await _fixture.ExecuteScopeAsync(async sp =>
+            {
+                var userManager = sp.GetService<UserManager<User>>();
+
+                var result = await DatabaseConfiguration
+                    .AddDefaultAdminAccountIfNoneExisting(userManager, configuration);
+
+                // Assert
+                result.Exists(credentials =>
+                        credentials.Email == expectedAdminCredentials.Email &&
+                        credentials.Password == expectedAdminCredentials.Password)
+                    .ShouldBeTrue();
+            });
+        }
+
+        [Fact]
+        public async Task ShouldNotCreateDefaultAdminAccountWhenExisting()
+        {
+            // Arrange
+            var configuration = await _fixture.ExecuteScopeAsync(async sp => sp.GetService<IConfiguration>());
+
+            var expectedAdminCredentials = configuration.GetAdminCredentials();
+
+            await _fixture.SendAsync(new Register
+            {
+                Id = Guid.NewGuid(),
+                Email = expectedAdminCredentials.Email,
+                Password = expectedAdminCredentials.Password,
+                FirstName = "Some",
+                LastName = "Account"
+            });
+
+            // Act
+            await _fixture.ExecuteScopeAsync(async sp =>
+            {
+                var userManager = sp.GetService<UserManager<User>>();
+
+                var result = await DatabaseConfiguration
+                    .AddDefaultAdminAccountIfNoneExisting(userManager, configuration);
+
+                // Assert
+                result.HasValue.ShouldBeFalse();
+            });
+        }
+    }
+}
