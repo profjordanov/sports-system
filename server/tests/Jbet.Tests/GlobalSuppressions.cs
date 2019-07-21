d3260ed89948bdd0ae327e32078d8aed1285ee72
Jordan Jordanov <d_yugioh@abv.bg>
2019-07-21 16:57:22 +0300

    sports-system v1.7.8

--- server/tests/Jbet.Tests/GlobalSuppressions.cs
+++ server/tests/Jbet.Tests/GlobalSuppressions.cs
@@ -5,4 +5,6 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "<Pending>", Scope = "member", Target = "~M:Jbet.Tests.TestHubConnection`1.#ctor(Microsoft.AspNetCore.SignalR.Client.HubConnection,System.String,System.Int32)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1309:Field names should not begin with underscore", Justification = "<Pending>", Scope = "member", Target = "~F:Jbet.Tests.Api.Hubs.TeamVotesHubTests._fixture")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1309:Field names should not begin with underscore", Justification = "<Pending>", Scope = "member", Target = "~F:Jbet.Tests.TestHubConnectionBuilder`1._eventName")]
 
