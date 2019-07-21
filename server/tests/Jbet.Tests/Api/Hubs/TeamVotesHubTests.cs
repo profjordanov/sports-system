e8cf00c600fdd23942e4277db265636e32b8a9a0
Jordan Jordanov <d_yugioh@abv.bg>
2019-07-21 18:27:25 +0300

    sports-system v1.7.9

--- server/tests/Jbet.Tests/Api/Hubs/TeamVotesHubTests.cs
+++ server/tests/Jbet.Tests/Api/Hubs/TeamVotesHubTests.cs
@@ -1,4 +1,12 @@
-﻿using Jbet.Domain.Events.Votes;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Jbet.Core.AuthContext.Commands;
+using Jbet.Core.VoteContext.Commands;
+using Jbet.Domain.Entities;
+using Jbet.Domain.Events.Votes;
+using Jbet.Tests.Business.AuthContext;
+using Jbet.Tests.Customizations;
+using Moq;
 using Xunit;
 
 namespace Jbet.Tests.Api.Hubs
@@ -6,18 +14,44 @@ namespace Jbet.Tests.Api.Hubs
     public class TeamVotesHubTests : ResetDatabaseLifetime
     {
         private readonly AppFixture _fixture;
+        private readonly AuthTestsHelper _authTestsHelper;
         private readonly string _hubUrl;
 
         public TeamVotesHubTests()
         {
             _fixture = new AppFixture();
+            _authTestsHelper = new AuthTestsHelper(_fixture);
             _hubUrl = _fixture.GetCompleteServerUrl("/teamVotes");
         }
 
-        [Fact]
-        public void Test()
+        [Theory]
+        [CustomizedAutoData]
+        public async Task UserTeamVotesAreSentToAllSubscribers(
+            Register registerCommand,
+            Team teamToVote)
         {
+            // Arrange
+            await _authTestsHelper.Register(registerCommand);
+
+            await _fixture.ExecuteDbContextAsync(async dbContext =>
+            {
+                await dbContext.Teams.AddAsync(teamToVote);
+                await dbContext.SaveChangesAsync();
+            });
+
             var testConnection = BuildTestConnection();
+
+            var voteForTeamCommand = new VoteForTeam(teamToVote.Id, registerCommand.Id);
+
+            // Act
+            await _fixture.SendAsync(voteForTeamCommand);
+
+            // Assert
+            testConnection
+                .VerifyMessageReceived(
+                    team => team.UserId == voteForTeamCommand.UserId &&
+                            team.TeamId == voteForTeamCommand.TeamId,
+                    Times.Never());
         }
 
         private TestHubConnection<UserVotedForTeam> BuildTestConnection() =>
