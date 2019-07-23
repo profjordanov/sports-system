4470f629229a2b66e4ff3a975f6d15a9f5a1e0e8
Jordan Jordanov <d_yugioh@abv.bg>
2019-07-24 00:11:23 +0300

    sports-system v1.8.3

---
+++ server/tests/Jbet.Tests/Business/TeamContext/Helpers/TeamTestsHelper.cs
@@ -0,0 +1,24 @@
+﻿using Jbet.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Jbet.Tests.Business.TeamContext.Helpers
+{
+    public class TeamTestsHelper
+    {
+        private readonly AppFixture _fixture;
+
+        public TeamTestsHelper(AppFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        public Task<Team> AddAsync(Team team) =>
+             _fixture.ExecuteDbContextAsync(
+                async context =>
+                {
+                    await context.Teams.AddAsync(team);
+                    await context.SaveChangesAsync();
+                    return team;
+                });
+    }
+}
