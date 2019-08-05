7a5b4b7eac33073597950629eeda7a91d78a7ffd
Jordan Jordanov <d_yugioh@abv.bg>
2019-08-05 13:52:21 +0300

    sports-system v2.0.6

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -4,6 +4,13 @@ import * as authApi from "../../api/authService";
 export function logoutSuccess() {
     return { type: ActionTypes.LOGOUT_SUCCESS };
 }
+export function loadCurrentUserSuccess(user) {
+    return { type: ActionTypes.LOAD_CURRENT_USER_SUCCESS, user };
+}
+
+export function loadCurrentUserFailure() {
+    return { type: ActionTypes.LOAD_CURRENT_USER_FAILURE };
+}
 
 export function login(credentials) {
     return function (dispatch) {
@@ -15,6 +22,19 @@ export function login(credentials) {
             .catch(error => {
                 dispatch(logout());
                 throw error;
+            });
+    };
+}
+
+export function loadCurrentUser() {
+    return function(dispatch) {
+        return authApi
+            .getCurrentUser()
+            .then(user => {
+                dispatch(loadCurrentUserSuccess(user));
+            })
+            .catch(() => {
+                dispatch(loadCurrentUserFailure());
             });
     };
 }
